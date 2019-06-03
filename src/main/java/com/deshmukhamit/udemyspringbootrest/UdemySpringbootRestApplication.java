e1ac5e9c5ab44f52e8febd02eb2e8576a1535dce
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-06-03 14:05:51 -0400

    first commit

---
+++ src/main/java/com/deshmukhamit/udemyspringbootrest/UdemySpringbootRestApplication.java
@@ -0,0 +1,15 @@
+package com.deshmukhamit.udemyspringbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(value = { "application.properties","mysql.application.properties" })
+public class UdemySpringbootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringbootRestApplication.class, args);
+	}
+
+}
