9f9c52de12f7b529cc732dfc0e9c8a74ae645741
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-06-04 22:33:25 -0400

    allowing CORS for HelloWorld controller

--- src/main/java/com/deshmukhamit/udemyspringbootrest/controller/HelloWorld.java
+++ src/main/java/com/deshmukhamit/udemyspringbootrest/controller/HelloWorld.java
@@ -1,11 +1,13 @@
 package com.deshmukhamit.udemyspringbootrest.controller;
 
 import com.deshmukhamit.udemyspringbootrest.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloWorld {
 
     @GetMapping("/hello-world")
