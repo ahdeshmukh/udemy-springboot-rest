c036455740ea9168348ae309e28024057d2ec5ad
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-07-31 23:08:37 -0400

    authenticating user against mysql database and returning jwt

--- src/main/java/com/deshmukhamit/udemyspringbootrest/jwt/JwtUserDetailService.java
+++ src/main/java/com/deshmukhamit/udemyspringbootrest/jwt/JwtUserDetailService.java
@@ -2,7 +2,9 @@
 
 package com.deshmukhamit.udemyspringbootrest.jwt;
 
-import org.springframework.security.core.userdetails.User;
+import com.deshmukhamit.udemyspringbootrest.user.DAOUser;
+import com.deshmukhamit.udemyspringbootrest.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,15 +15,27 @@ import java.util.ArrayList;
 @Service
 public class JwtUserDetailService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Password generated at https://bcrypt-generator.com/ for string "password"
-        // Todo: instead of single value john.doe, use array with multiple user names, password can remain the same
-        if("john.doe".equals(username)) {
-            return new User(username,"$2a$10$mooHRvqfH.TNjQcQoi2RdeQ8xtbTHRCq9cRlAhjKvlNfessvJXDBW",
-                    new ArrayList<>());
-        } else {
+
+        DAOUser user = userRepository.findByUsername(username);
+        if(user == null) {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+
+        // Password generated at https://bcrypt-generator.com/ for string "password"
+        // Todo: instead of single value john.doe, use array with multiple user names, password can remain the same
+//        if("john.doe".equals(username)) {
+//            return new User(username,"$2a$10$mooHRvqfH.TNjQcQoi2RdeQ8xtbTHRCq9cRlAhjKvlNfessvJXDBW",
+//                    new ArrayList<>());
+//        } else {
+//            throw new UsernameNotFoundException("User not found with username: " + username);
+//        }
     }
 }
